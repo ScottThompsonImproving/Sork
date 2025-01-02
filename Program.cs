290b4307ff20b5b41ecf79f9932bb9fbb3fcea30
Scott Thompson <scott.thompson@improving.com>
2025-01-02 17:52:08 -0600

    Add 3 new commands

--- Program.cs
+++ Program.cs
@@ -9,6 +9,9 @@
             input = input.ToLower();
             input = input.Trim();
             if (input == "lol") { Console.WriteLine("You laugh out loud!"); }
+            else if (input == "dance") { Console.WriteLine("You spin around in circles!"); }
+            else if (input == "sing") { Console.WriteLine("You wail like a banshee!"); }
+            else if (input == "whistle") { Console.WriteLine("You sound like a boiling tea kettle!"); }
             else if (input == "exit") { break; }
             else { Console.WriteLine("Unkown command"); }
         } while (true);
